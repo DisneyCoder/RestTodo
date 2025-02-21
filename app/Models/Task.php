7ed52a9ffc0327c8ac95827de8280e6f5ca54df5
AANoman <noman.2863p@gmail.com>
2025-02-21 10:34:50 +0600

    create migration and factory

---
+++ app/Models/Task.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Task extends Model {
+    /** @use HasFactory<\Database\Factories\TaskFactory> */
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected function cast() {
+        return [
+            'is_finished' => 'boolean',
+            'finished_at' => 'datetime',
+        ];
+    }
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
